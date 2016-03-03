f781e5a2052872870bfd69796cfd579491e71768
LEMONKEBAB <benmiller105@gmail.com>
2016-03-03 14:34:26 +0000

    Can manually update product quantity through Java

--- src/IMS.java
+++ src/IMS.java
@@ -16,7 +16,7 @@ public class IMS {
 	char[] Chararr = {1, 2, 3, 4};
 	public static void main(String[] args) {
 		IMS ims = new IMS();
-		ims.select();
+		ims.update();
 		
 	}
 	
@@ -24,22 +24,11 @@ public class IMS {
 		query.readProduct(new ProductM(), "localhost", 27017, "NBmk2");
 	}
 	public void select(){
-		//query.selectEntity(new Customer(), url, "SYSTEM", "1234");
-		//query.selectEntity(new Address(), url, "SYSTEM", "1234");
+		query.selectEntity(new Customer(), url, "SYSTEM", "1234");
+		query.selectEntity(new Address(), url, "SYSTEM", "1234");
 		query.selectEntity(new Product(), url, "SYSTEM", "1234");
-		/*try {
-			while(rs.next()) {
-				System.out.println(rs.getInt("customerid")+" "+
-				rs.getString("fullname")+" "+rs.getString("dateofbirth")+" "+
-				rs.getString("status"));
-			 }
-		} catch (SQLException e) {
-				 e.printStackTrace();
-	}*/
-
-		//Connect connect = new Connect();
-		//ResultSet select = connect.selectEntity("Address" ,url, "SYSTEM", "1234"); 
-		
 	}
-
+	public void update(){
+		query.updateProduct(url, "SYSTEM", "1234", "-", 3, "productID = 1");
+	}
 }
--- src/Query.java
+++ src/Query.java
@@ -80,24 +80,47 @@ public class Query {
 			return rs;
 		}
 	
-	public <E> void addEntity(E e, String host, int port, String dbn, String user, char[] pass) {
-			 MongoClient client = new MongoClient(host, port);
-			 @SuppressWarnings("deprecation")
-			DB db = client.getDB(dbn);
-			 DBCollection col = db.getCollection(
+	public ResultSet updateProduct(String url, String user, String pass, String pm, int i, String query) {
+		ResultSet rs = null;
+		Connection conn = null;
+		Statement stat = null;
+			try {
+				conn = connect.getConn(url, user, pass);
+				System.out.println("Connected");
+				stat = conn.createStatement(
+						ResultSet.TYPE_SCROLL_SENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
+			 rs = stat.executeQuery("UPDATE PRODUCT SET QUANTITY = QUANTITY " + pm + i + " WHERE " + query);
+			 
+				 
+			 }catch (SQLException se) {
+				se.printStackTrace();
+			} finally {
+				connect.closeConn(stat, conn);
+				System.out.println("Disconnected");
+			}
+			return rs;
+		}
+	
+	public <E> void addEntity(E e, String host, int port, String dbn) {
+		MongoClient client = new MongoClient(host, port);
+		@SuppressWarnings("deprecation")
+		DB db = client.getDB(dbn);
+		DBCollection col = db.getCollection(
 			e.getClass().getName(). substring(e.getClass().
 			getName().lastIndexOf('.') + 1));
-			 try {
-			 WriteResult r = col.insert((DBObject[]) e);
-			 System.out.println(r.getUpsertedId());
-			 System.out.println(r.getN());
-			 System.out.println(r.isUpdateOfExisting());
-			 } catch(ClassCastException cce) {
-			 cce.printStackTrace();
-			 } finally {
-			 client.close();
-			 }
-			}
+		try {
+			WriteResult r = col.insert((DBObject[]) e);
+			System.out.println(r.getUpsertedId());
+			System.out.println(r.getN());
+			System.out.println(r.isUpdateOfExisting());
+		} catch(ClassCastException cce) {
+			cce.printStackTrace();
+		} finally {
+			client.close();
+		}
+	}
+	
 	public <E> void readProduct(E a,String host, int port, String dbn) {
 		MongoClient client = new MongoClient(host, port);
 		try{
